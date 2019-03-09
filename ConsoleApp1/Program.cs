6a504eb81e7a47e139b6aff363db7ec4c7a614d3
visualstudio <visualstudio@122-PC33>
2019-03-08 20:50:42 -0500

    Lab 12

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,24 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 namespace DelegatesAndEvents
 {
-    public class Program
+    using System;
 
-       public delegate void MyDelegate();
-        public class DelegateExercises
+    delegate void ExampleDelegate(string xyz);
+
+    class Program
+    {
+        public static void Method1(string xyz)
         {
-            void Method3()
+            Console.WriteLine(xyz + " Method1");
+        }
 
-          
-            {
-            
-                Console.WriteLine(MyDelegate.ToString());
-            }
+        public static void Method2(string xyz)
+        {
+            Console.WriteLine(xyz + " Method2");
+        }
+
+        public static void Main()
+        {
+            ExampleDelegate ex1Delegate, ex2Delegate, ex3Delegate, myDelegate;
+
+            ex1Delegate = new ExampleDelegate(Method1);
+            ex2Delegate = new ExampleDelegate(Method2);
+            ex3Delegate = ex1Delegate + ex2Delegate;
+            myDelegate = ex1Delegate - ex2Delegate;
+            ex1Delegate("AAA");
+            ex2Delegate("BBB");
+            ex3Delegate("CCC");
+            myDelegate("DDD");
+            myDelegate = ex3Delegate - ex1Delegate;
+            myDelegate("EEE");
+            myDelegate = ex3Delegate - ex2Delegate;
+            myDelegate("FFF");
+            Console.ReadLine();
         }
     }
-
-
+}
 
