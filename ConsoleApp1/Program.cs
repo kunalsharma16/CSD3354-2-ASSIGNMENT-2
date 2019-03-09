75115f5dbcfa61f64c8d38af5b24abb591e000d4
visualstudio <visualstudio@122-PC33>
2019-03-08 19:29:51 -0500

    Lab 05

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,37 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-///kunal sharma C0727250
-// Sohit sharma C0730478
 namespace DelegatesAndEvents
 {
+    public class Program
+    {
+        public static void Main()
+        {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
+        }
+    }
+
     public class DelegateExercises
     {
-        public delegate void MyDelegate(int value);
+        public delegate int MyDelegate();
 
-        public static void Main(String[] args)
+        void Method1()
         {
-            MyDelegate myDelegate = Method1;
-            myDelegate+=Method3;
-            myDelegate(50);
+            Console.WriteLine("MyDelegate");
+           
+
         }
 
-        public static void Method1(int num1)
-        {
-            Console.WriteLine(num1 * 2);
-        }
+        public void Method3()
 
-        public static void Method3(int num2)
         {
-            Console.WriteLine(num2 * 10);
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+               
         }
-        public static void Method2(int x)
-        {
-            Console.WriteLine("Method2");
-            Console.WriteLine("Number is "+x);
-            Console.ReadLine();
-        }
-
         
 
     
